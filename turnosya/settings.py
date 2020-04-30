881e41bb11af9b38644070d16eda70a43be17c15
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-04-30 00:39:41 -0300

    setting new database engine

--- turnosya/settings.py
+++ turnosya/settings.py
@@ -87,9 +87,17 @@ WSGI_APPLICATION = 'turnosya.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'turnosya',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
