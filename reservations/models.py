e9870a29af8d922562db45834065be78f243ce6f
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-05-04 20:08:27 -0300

    adding new models to the reservations app

--- reservations/models.py
+++ reservations/models.py
@@ -5,3 +5,49 @@ from commerces.models import Commerce
 from courts.models import Court
 from services.models import Service
 from employees.models import Employee
+
+
+class ReservationState(models.Model):
+    id = models.CharField(max_length=100, primary_key=True, unique=True)
+    name = models.CharField(max_length=100)
+    softDelete = models.DateTimeField(blank=True, null=True)
+
+
+class PaymentMethod(models.Model):
+    id = models.CharField(max_length=100, primary_key=True, unique=True)
+    name = models.CharField(max_length=100)
+    softDelete = models.DateTimeField(blank=True, null=True)
+
+
+class Payment(models.Model):
+    paymentMethodId = models.ForeignKey(PaymentMethod, on_delete=models.SET_NULL)
+    paymentDate = models.DateTimeField()
+    refundDate = models.DateTimeField(blank=True, null=True)
+    receiptNumber = models.CharField(max_length=100, blank=True, null=True)
+
+
+class Reservation(models.Model):
+    commerceId = models.ForeignKey(Commerce, on_delete=models.CASCADE)
+    clientId = models.ForeignKey(Profile, on_delete=models.SET_NULL)
+    courtId = models.ForeignKey(Court, on_delete=models.SET_NULL, blank=True, null=True)
+    serviceId = models.ForeignKey(Service, on_delete=models.SET_NULL, blank=True, null=True)
+    employeeId = models.ForeignKey(Employee, on_delete=models.SET_NULL, blank=True, null=True)
+    paymentId = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
+    stateId = models.ForeignKey(ReservationState, on_delete=models.SET_NULL)
+    clientName = models.CharField(max_length=100)
+    clientPhone = models.CharField(max_length=100)
+    price = models.FloatField()
+    reservationDate = models.DateTimeField()
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    cancellationDate = models.DateTimeField(blank=True, null=True)
+
+
+class Review(models.Model):
+    reservationId = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    commerceId = models.ForeignKey(Commerce, on_delete=models.SET_NULL, blank=True, null=True)
+    clientId = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    rating = models.FloatField()
+    comment = models.TextField()
+    reviewDate = models.DateTimeField()
+    softDelete = models.DateTimeField(blank=True, null=True)
